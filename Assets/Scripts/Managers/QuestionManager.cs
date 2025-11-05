4c41b2248d6d5f4276b5d1e3abd86b7d73f0132c
SebGit-dev <hikup.github@gmail.com>
2025-11-05 09:51:00 -0500

    UI finished

--- Assets/Scripts/Managers/QuestionManager.cs
+++ Assets/Scripts/Managers/QuestionManager.cs
@@ -1,8 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class QuestionManager : MonoBehaviour
 {
     public GameManager gameManager;
+
+    [Header("Resultat Indices Couleurs")]
+    public TMP_Text resultatIndiceRed;
+    public TMP_Text resultatIndiceBlack;
+
+    [Header("Resultat Indices Figures")]
+    public TMP_Text resultatIndiceKings;
+    public TMP_Text resultatIndiceQueens;
+    public TMP_Text resultatIndiceJacks;
+    public TMP_Text resultatIndiceAces; 
+
+    [Header("Resultat Indices Symboles")]
+    public TMP_Text resultatIndiceHearts;
+    public TMP_Text resultatIndiceDiamonds;
+    public TMP_Text resultatIndiceSpades;
+    public TMP_Text resultatIndiceClubs;
 
     public void QuestionColorRed()
     {
@@ -18,6 +36,7 @@ public class QuestionManager : MonoBehaviour
 
         // Optionally do something with totalBlack:
         Debug.Log("Total black cards: " + totalRed);
+        resultatIndiceRed.text = totalRed.ToString();
     }
     public void QuestionColorBlack()
     {
@@ -33,6 +52,7 @@ public class QuestionManager : MonoBehaviour
 
         // Optionally do something with totalBlack:
         Debug.Log("Total black cards: " + totalBlack);
+        resultatIndiceBlack.text = totalBlack.ToString();
     }
 
     public void QuestionValue(string FigureButtonName)
@@ -61,21 +81,25 @@ public class QuestionManager : MonoBehaviour
                 totalAce++;
             }
         }
-        if(FigureButtonName == "KingButton")
+        if(FigureButtonName == "IndicesKings")
         {
             Debug.Log("Total King cards: " + totalKing);
+            resultatIndiceKings.text = totalKing.ToString();
         }
-        else if(FigureButtonName == "QueenButton")
+        else if(FigureButtonName == "IndicesQueens")
         {
             Debug.Log("Total Queen cards: " + totalQueen);
+            resultatIndiceQueens.text = totalQueen.ToString();
         }
-        else if(FigureButtonName == "JackButton")
+        else if(FigureButtonName == "IndicesJacks")
         {
             Debug.Log("Total Jack cards: " + totalJack);
+            resultatIndiceJacks.text = totalJack.ToString();
         }
-        else if(FigureButtonName == "AceButton")
+        else if(FigureButtonName == "IndicesAces")
         {
             Debug.Log("Total Ace cards: " + totalAce);
+            resultatIndiceAces.text = totalAce.ToString();
         }
     }
     
@@ -106,21 +130,25 @@ public class QuestionManager : MonoBehaviour
             }
         }
 
-        if(SymbolButtonName == "HeartsButton")
+        if(SymbolButtonName == "IndicesHearts")
         {
             Debug.Log("Total Heart cards: " + totalHearts);
+            resultatIndiceHearts.text = totalHearts.ToString();
         }
-        else if(SymbolButtonName == "DiamondsButton")
+        else if(SymbolButtonName == "IndicesDiamonds")
         {
             Debug.Log("Total Diamonds cards: " + totalDiamonds);
+            resultatIndiceDiamonds.text = totalDiamonds.ToString();
         }
-        else if(SymbolButtonName == "SpadesButton")
+        else if(SymbolButtonName == "IndicesSpades")
         {
             Debug.Log("Total Spades cards: " + totalSpades);
+            resultatIndiceSpades.text = totalSpades.ToString();
         }
-        else if(SymbolButtonName == "ClubsButton")
+        else if(SymbolButtonName == "IndicesClubs")
         {
             Debug.Log("Total Clubs cards: " + totalClubs);
+            resultatIndiceClubs.text = totalClubs.ToString();
         }
     }
 }
